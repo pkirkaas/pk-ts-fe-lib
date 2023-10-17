d3ba1be3506c083766e0a7d04322891cb9a86739
Paul Kirkaas <pkirkaas@gmail.com>
2023-10-17 22:00:20 +0200

    Some tweaks to get stuff to work, need to fine tune

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# A Front End (React & SASS) TS Library, and testing platform
+# A Front End (React & SASS) TS Library, and testing platform, trying Vite (for now...)
 
-## Components exported in init.js/ts
+
+## Copyright Paul Kirkaas (pkirkaas@gmail.com): 17-Oct-2023 21:42
+
+## Basics
+
+A very experimental fe library using React & Vite to build the front end, and the Pk-Ts-Common library
+
+Not sure Vite is the way to go, and things change fast, so all has to be double-checked & updated.
+
+## Build & Test
+
+As of 17 Oct 2023 -
+
+Run:
+
+```bash
+ npm run clean
+ npm run build
+ npm run preview
+ ```
+
+ Not as it should be, for now with EACH change of .tsx, whatever, have to re-run BOTH:
+
+```bash
+ npm run build
+ npm run preview
+ ```
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -10,10 +10,16 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
+          <img src="./vite.svg" className="logo" alt="Vite logo" />
+          { /*
           <img src={viteLogo} className="logo" alt="Vite logo" />
+  */ }
         </a>
         <a href="https://reactjs.org" target="_blank">
+          <img src="./react.svg" className="logo react" alt="React logo" />
+          { /*
           <img src={reactLogo} className="logo react" alt="React logo" />
+  */ }
         </a>
       </div>
       <h1>Vite + React</h1>
