644677e41ed6a66e5a90e0dc985e4dfebe30ba89
Paul Kirkaas <pkirkaas@gmail.com>
2024-06-13 20:29:47 +0200

    autosync commit from GOA: MSG: Got combining all flex-display configs into flat object - but have to clean it up

--- dist/esm/libs/sbStyles.js
+++ dist/esm/libs/sbStyles.js
@@ -7,6 +7,8 @@
  * 24-Jun-11 18:15
  *
  */
+import { dotPathVal, toCamel, camelKeys, } from 'pk-ts-common-lib';
+import _ from 'lodash';
 import { StyleBuilder, } from './styleUtils.js';
 export let sbStyles = {};
 /**
@@ -26,7 +28,8 @@ export function buildFlexStylesOld(sObj = sbStyles) {
             let fvVal = fdVal[fvKey];
             fsNameArr.push(`${fdKey}${fvKey}`);
             //_.merge(tmpStyle, fvVal);
-            console.log(`Inner Loop:`, { fdKey, fvKey, fvVal, fsNameArr,
+            console.log(`Inner Loop:`, {
+                fdKey, fvKey, fvVal, fsNameArr,
                 //   tmpStyle,
             });
             //console.log(`buildFlexStyles:`,{fdKey, fdVal, fvKey, fvVal});
@@ -37,6 +40,17 @@ export function buildFlexStylesOld(sObj = sbStyles) {
         console.log(tmpKey, tmpStyle);
     }
 }
+//function cartesianProduct(arrays) {
+function cartesianProduct(...arrays) {
+    // Initialize with an empty array within an array
+    return arrays.reduce((acc, array) => {
+        // If array is empty, just return the accumulated results so far
+        if (array.length === 0)
+            return acc;
+        // Accumulate combinations of current result and new array
+        return acc.flatMap(accElem => array.map(elem => [...accElem, elem]));
+    }, [[]]);
+}
 export function buildFlexStyles(sObj = sbStyles) {
     //NO CIGAR!
     let flexStyles = {};
@@ -44,25 +58,42 @@ export function buildFlexStyles(sObj = sbStyles) {
     let fsNameArr = [];
     // let recurse = function(keyArr,
     for (let fdKey in flexDisplays) {
-        let tmpStyle = {};
+        let tmpArr = [];
+        //let tmpStyle: GenObj = {};
         let fdVal = flexDisplays[fdKey];
         for (let fvKey in fdVal) {
+            //  let tmpArr = [];
+            //  let tmpArr2 = [];
             //      let fsNameArr = [];
             //      let tmpStyle:GenObj = {};
             let fvVal = fdVal[fvKey];
-            let keyStr = `${fdKey}${fvKey}`;
-            fsNameArr.push(`${fdKey}${fvKey}`);
-            //_.merge(tmpStyle, fvVal);
-            console.log(`Inner Loop:`, { fdKey, fvKey, fvVal, fsNameArr,
-                //   tmpStyle,
-            });
-            //console.log(`buildFlexStyles:`,{fdKey, fdVal, fvKey, fvVal});
-            //console.log(`buildFlexStyles:`,{fdKey, fdVal, fvKey, });
-            //      console.log(`bfs:`,`${fdKey}-${fvKey}`);
+            let keyStr = `${fdKey}.${fvKey}`;
+            tmpArr.push(keyStr);
         }
-        let tmpKey = fsNameArr.join('-');
-        console.log(tmpKey, tmpStyle);
+        fsNameArr = cartesianProduct(fsNameArr, tmpArr);
     }
+    let flatArr = [];
+    for (let fsNA of fsNameArr) {
+        flatArr.push(fsNA.flat(Infinity));
+    }
+    let fstyleObj = {};
+    for (let row of flatArr) {
+        //let fsSKey = toCamel(row.join('-').replaceAll('.'));
+        let tmpStyle = {};
+        let fsSKey = toCamel(row.join('-').replaceAll('\.', ''));
+        let retArr = [];
+        for (let el of row) {
+            let dispS = dotPathVal(flexDisplays, el);
+            tmpStyle = _.merge(tmpStyle, dispS);
+            retArr.push(dispS);
+        }
+        //fstyleObj[fsSKey] = "TmpVal"; 
+        //fstyleObj[fsSKey] = retArr;
+        fstyleObj[fsSKey] = tmpStyle;
+    }
+    fstyleObj = camelKeys(fstyleObj);
+    //console.log(`bsf`, {fsNameArr});
+    console.log(`bsf`, { fstyleObj });
 }
 sbStyles.frss = StyleBuilder.builder.d('ais', 'jcs').clone;
 sbStyles.fcss = sbStyles.frss.d('c').clone;
--- src/libs/sbStyles.ts
+++ src/libs/sbStyles.ts
@@ -10,7 +10,7 @@
 
 import {
   getProps, getObjDets, subObj, typeOf, allProps, allPropsP, objInfo,
-  GenObj, isNumeric, isSimpleObject,
+  GenObj, isNumeric, isSimpleObject, dotPathVal, toCamel, camelKeys,
 
 } from 'pk-ts-common-lib';
 
@@ -45,8 +45,9 @@ export function buildFlexStylesOld(sObj: GenObj = sbStyles) { // Build combinati
       let fvVal = fdVal[fvKey];
       fsNameArr.push(`${fdKey}${fvKey}`);
       //_.merge(tmpStyle, fvVal);
-      console.log(`Inner Loop:`, {fdKey, fvKey, fvVal, fsNameArr,
-     //   tmpStyle,
+      console.log(`Inner Loop:`, {
+        fdKey, fvKey, fvVal, fsNameArr,
+        //   tmpStyle,
       });
 
       //console.log(`buildFlexStyles:`,{fdKey, fdVal, fvKey, fvVal});
@@ -57,36 +58,66 @@ export function buildFlexStylesOld(sObj: GenObj = sbStyles) { // Build combinati
     console.log(tmpKey, tmpStyle);
   }
 }
+
+//function cartesianProduct(arrays) {
+function cartesianProduct(...arrays) {
+  // Initialize with an empty array within an array
+  return arrays.reduce((acc, array) => {
+    // If array is empty, just return the accumulated results so far
+    if (array.length === 0) return acc;
+
+    // Accumulate combinations of current result and new array
+    return acc.flatMap(accElem => array.map(elem => [...accElem, elem]));
+  }, [[]]);
+}
+
 
 export function buildFlexStyles(sObj: GenObj = sbStyles) { // Build combinations of flex display styles
   //NO CIGAR!
   let flexStyles: GenObj = {};
   let flexDisplays = StyleBuilder.flexDisplays;
   let fsNameArr = [];
- // let recurse = function(keyArr,
+  // let recurse = function(keyArr,
   for (let fdKey in flexDisplays) {
-    let tmpStyle: GenObj = {};
+    let tmpArr = [];
+    //let tmpStyle: GenObj = {};
     let fdVal = flexDisplays[fdKey];
     for (let fvKey in fdVal) {
+      //  let tmpArr = [];
+      //  let tmpArr2 = [];
       //      let fsNameArr = [];
       //      let tmpStyle:GenObj = {};
       let fvVal = fdVal[fvKey];
-      let keyStr = `${fdKey}${fvKey}`;
-      fsNameArr.push(`${fdKey}${fvKey}`);
-      //_.merge(tmpStyle, fvVal);
-      console.log(`Inner Loop:`, {fdKey, fvKey, fvVal, fsNameArr,
-     //   tmpStyle,
-      });
-
-      //console.log(`buildFlexStyles:`,{fdKey, fdVal, fvKey, fvVal});
-      //console.log(`buildFlexStyles:`,{fdKey, fdVal, fvKey, });
-      //      console.log(`bfs:`,`${fdKey}-${fvKey}`);
+      let keyStr = `${fdKey}.${fvKey}`;
+      tmpArr.push(keyStr);
     }
-    let tmpKey = fsNameArr.join('-');
-    console.log(tmpKey, tmpStyle);
+    fsNameArr = cartesianProduct(fsNameArr, tmpArr);
   }
-}
+  let flatArr = [];
+  for (let fsNA of fsNameArr) {
+    flatArr.push(fsNA.flat(Infinity));
+  }
+  let fstyleObj:GenObj = {};
+  for (let row of flatArr) {
+    //let fsSKey = toCamel(row.join('-').replaceAll('.'));
+    let tmpStyle:GenObj = {};
+    let fsSKey = toCamel(row.join('-').replaceAll('\.',''));
+    let retArr = [];
+    for (let el of row) {
+      let dispS = dotPathVal(flexDisplays, el);
+      tmpStyle = _.merge(tmpStyle, dispS);
+      retArr.push(dispS);
+    }
+    //fstyleObj[fsSKey] = "TmpVal"; 
+    //fstyleObj[fsSKey] = retArr;
+    fstyleObj[fsSKey] = tmpStyle;
+  }
+  fstyleObj = camelKeys(fstyleObj);
+  //console.log(`bsf`, {fsNameArr});
+  console.log(`bsf`, { fstyleObj});
 
+
+}
 
 
 sbStyles.frss = StyleBuilder.builder.d('ais', 'jcs').clone;
