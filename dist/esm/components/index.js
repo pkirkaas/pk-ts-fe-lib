85bc0c805fafcef8a8ce0d08b825b9dc5d10aeeb
Paul Kirkaas <pkirkaas@gmail.com>
2024-12-09 05:21:26 +0100

    autosync commit from GOA: MSG: Trying to get css modules to work as exported

--- dist/esm/components/index.js
+++ dist/esm/components/index.js
@@ -3,4 +3,5 @@ export * from './formComponents.js';
 export * from './utilComponents.js';
 export * from './daisyui/index.js';
 export * from './resizable-panels/resizable-components.js';
+//export * from './resizable-panels/resizable.module.css";
 //# sourceMappingURL=index.js.map
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export * from './formComponents.js';
 export * from './utilComponents.js';
 export * from './daisyui/index.js';
 export * from './resizable-panels/resizable-components.js';
+//export * from './resizable-panels/resizable.module.css";
